764c96ddc3a752336a3d9a5acaed0edf6abe1ef9
cumet04 <medalhkr@gmail.com>
2022-02-12 13:13:41 +0900

    multi device

--- src/Storyshots.test.ts
+++ src/Storyshots.test.ts
@@ -1,33 +1,43 @@
 import initStoryshots from '@storybook/addon-storyshots';
-import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
-import { Browser, chromium, devices } from 'playwright'
+import { imageSnapshot, Context } from '@storybook/addon-storyshots-puppeteer';
+import { Browser, BrowserType, chromium, webkit, firefox, devices } from 'playwright'
 import path from 'path'
 
+// deviceName: refs https://github.com/microsoft/playwright/blob/v1.19.0/packages/playwright-core/types/types.d.ts#L15739
+function initBrowserStoryshots(key: string, deviceName: string, browserType: BrowserType) {
+  let browser: Browser;
+  afterAll(() => {
+    return browser.close();
+  });
 
-let browser: Browser;
-afterAll(() => {
-  return browser.close();
-});
+  // MEMO: puppeteerパッケージを入れているとここで型エラーになる
+  const getCustomBrowser = async () => {
+    browser = await browserType.launch()
+    const context = await browser.newContext(devices[deviceName])
+    return context
+  }
 
-initStoryshots({
-  test: imageSnapshot({
-    storybookUrl: `file://${path.resolve(__dirname, '../storybook-static')}`,
-    // MEMO: puppeteerパッケージを入れているとここで型エラーになる
-    getCustomBrowser: async () => {
-      browser = await chromium.launch()
-      const context = await browser.newContext(devices['Pixel 5'])
-      return context
-    },
-    getMatchOptions: (options) => {
-      const { kind, story } = options.context
+  const getMatchOptions = (options: {context: Context, url: string}) => {
+    const { kind, story } = options.context
 
-      const dir = path.resolve(__dirname, '__image_snapshots__', kind)
-      const name = story.replaceAll(/\s/g, '')
+    const dir = path.resolve(__dirname, '__image_snapshots__', key, kind)
+    const name = story.replaceAll(/\s/g, '')
 
-      return {
-        customSnapshotsDir: path.resolve(dir),
-        customSnapshotIdentifier: name,
-      };
-    },
-  })
-});
+    return {
+      customSnapshotsDir: path.resolve(dir),
+      customSnapshotIdentifier: name,
+    }
+  }
+
+  initStoryshots({
+    test: imageSnapshot({
+      storybookUrl: `file://${path.resolve(__dirname, '../storybook-static')}`,
+      getCustomBrowser,
+      getMatchOptions,
+    })
+  });
+}
+
+initBrowserStoryshots('Desktop-firefox', "Desktop Firefox", firefox)
+initBrowserStoryshots('Pixel5-chrome', "Pixel 5", chromium)
+initBrowserStoryshots('iPhone13-safari', "iPhone 13", webkit)
