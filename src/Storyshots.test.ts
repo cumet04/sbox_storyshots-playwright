70b066ada1288dcf3d8364c182eae3d2e6733dc1
cumet04 <medalhkr@gmail.com>
2022-02-12 12:09:06 +0900

    use local file

--- src/Storyshots.test.ts
+++ src/Storyshots.test.ts
@@ -1,9 +1,11 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 import playwright from 'playwright'
+import path from 'path'
 
 initStoryshots({
   test: imageSnapshot({
+    storybookUrl: `file://${path.resolve(__dirname, '../storybook-static')}`,
     getCustomBrowser: () => {
       return playwright.chromium.launch()
     }
