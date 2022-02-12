c7aa380b7cdcf8f9a857f7d2f95dccdb84755093
cumet04 <medalhkr@gmail.com>
2022-02-12 12:14:30 +0900

    close browser

--- src/Storyshots.test.ts
+++ src/Storyshots.test.ts
@@ -3,11 +3,18 @@ import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 import playwright from 'playwright'
 import path from 'path'
 
+let browser: playwright.Browser;
+afterAll(() => {
+  return browser.close();
+});
+
 initStoryshots({
   test: imageSnapshot({
     storybookUrl: `file://${path.resolve(__dirname, '../storybook-static')}`,
-    getCustomBrowser: () => {
-      return playwright.chromium.launch()
+    // MEMO: puppeteerパッケージを入れているとここで型エラーになる
+    getCustomBrowser: async () => {
+      browser = await playwright.chromium.launch()
+      return browser
     }
   })
 });
