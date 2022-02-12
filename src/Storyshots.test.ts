36b3d896727812f5252454ed39b690bbd2596364
cumet04 <medalhkr@gmail.com>
2022-02-12 10:59:49 +0900

    storyshots-playwright
    npm install -D @storybook/addon-storyshots-puppeteer playwright
    https://github.com/storybookjs/storybook/issues/10162#issuecomment-690423930

--- src/Storyshots.test.ts
+++ src/Storyshots.test.ts
@@ -1,3 +1,11 @@
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+import playwright from 'playwright'
 
-initStoryshots();
+initStoryshots({
+  test: imageSnapshot({
+    getCustomBrowser: () => {
+      return playwright.chromium.launch()
+    }
+  })
+});
